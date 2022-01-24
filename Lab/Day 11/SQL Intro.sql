d46ec3df9f22b6d1d80d7f2ae9c5f2e6def678ff
lmbauyeung <97063729+lmbauyeung@users.noreply.github.com>
2022-01-24 18:25:57 +0100

    Creat SQL Intro Lab

---
+++ Lab/Day 11/SQL Intro.sql
@@ -0,0 +1,14 @@
+-- Q3
+SELECT title FROM sakila.film;
+
+-- Q4
+SELECT DISTINCT name FROM sakila.language;
+
+-- Q5.1
+SELECT COUNT(*) FROM sakila.store;
+
+-- Q5.2
+SELECT COUNT(*) FROM sakila.staff;
+
+-- Q5.3
+SELECT first_name FROm sakila.staff;
